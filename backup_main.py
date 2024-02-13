a822c4974a49e1f38944d1d41617d9f32b59320a
tlorne96 <timlorne96@gmail.com>
2024-02-14 09:49:47 +1100

    testing

--- backup_main.py
+++ backup_main.py
@@ -1,73 +1,32 @@
-import network
-import socket
-from time import sleep
-from picozero import pico_temp_sensor, pico_led
+import _thread
+from PiicoDev_SSD1306 import create_PiicoDev_SSD1306
 import machine
-ssid = 'Solus1'
-password = 'thinkb81'
+import utime
+import math
 
-def connect():
-    #Connecting to network
-    wlan = network.WLAN(network.STA_IF)
-    wlan.active(True)
-    wlan.connect(ssid, password)
-    while wlan.isconnected()== False:
-        print('waiting for connection...')
-        sleep(1)
-    ip = wlan.ifconfig()[0]
-    print(f'connected on {ip}')
-    return ip #returning IP address as a variable
+temp_data = 0.0
 
-#creating a function to open a socket
-def open_socket(ip):
-    adress = (ip, 80)
-    connection = socket.socket()
-    connection.bind(adress)
-    connection.listen(1)
-    return(connection)
+sensor_temp = machine.ADC(4)
+conversion_factor = 3.3 / 65535
+display = create_PiicoDev_SSD1306()
 
-def webpage(temperature, state):
-    #template html
-    html = f"""
-            <!DOCTYPE html>
-            <html>
-            <body>
-            <form action="./lighton">
-            <input type="submit" value="Light on">
-            </form>
-            <form action="./lightoff">
-            <input type="submit" value="Light off">
-            </form>
-            <p>LED is {state}</p>
-            <p>Temp is {temperature}</p>
-            </body>
-            </html>
-            """
-    return str(html)
-def serve(connection):
-    state = 'OFF'
-    pico_led.off()
-    temperature = 0
-    while True:
-        client = connection.accept()[0]
-        request = client.recv(1024)
-        request = str(request)
-        print(request)
-        try:
-            request = request.split()[1]
-        except IndexError:
-            pass
-        if request == '/lighton?':
-            pico_led.on()
-        elif request =='/lightoff?':
-            pico_led.off()
-        html = webpage(temperature, state)
-        client.send(html)
-        client.close()
+def display_data(temperature):
+    display.text("The current Temperature is", 0, 0, 1)
+    display.text(str(temperature), 0, 15, 1)
+    display.show()
 
-try:
-    ip = connect()
-    connection = open_socket(ip)
-    serve(connection)
-except KeyboardInterrupt:
-    machine.reset()
+
+def get_temp_data():
+    reading = sensor_temp.read_u16() * conversion_factor
+    ug_temp = 27 - (reading - 0.706) / 0.001721
+    temperature = math.trunc(ug_temp)
+    return temperature
+
+_thread.start_new_thread(display_data, (temp_data,))
+
+while True:
+    temp_data = get_temp_data()
+    display_data(temp_data)
+    utime.sleep(2)
+    display.fill(0)
+    display.show()
--- main.py
+++ main.py
@@ -1,7 +1,32 @@
-from machine import Pin
-from time import sleep
-pin = Pin("LED", Pin.OUT)
+import _thread
+from PiicoDev_SSD1306 import create_PiicoDev_SSD1306
+import machine
+import utime
+import math
+
+temp_data = 0.0
+
+sensor_temp = machine.ADC(4)
+conversion_factor = 3.3 / 65535
+display = create_PiicoDev_SSD1306()
+
+def display_data(temperature):
+    display.text("The current Temperature is", 0, 0, 1)
+    display.text(str(temperature), 0, 15, 1)
+    display.show()
+
+
+def get_temp_data():
+    reading = sensor_temp.read_u16() * conversion_factor
+    ug_temp = 27 - (reading - 0.706) / 0.001721
+    temperature = math.trunc(ug_temp)
+    return temperature
+
+_thread.start_new_thread(display_data, (temp_data,))
 
 while True:
-    pin.toggle()
-    sleep(1)
+    temp_data = get_temp_data()
+    display_data(temp_data)
+    utime.sleep(2)
+    display.fill(0)
+    display.show()
